3cf30f0277a540c2c4654bbc13231782f014db86
congzhaoyang <congzhaoyang1@163.com>
2017-10-20 11:20:01 +0800

    [update] 在添加chunks前一步，可运行

--- webpack.config.js
+++ webpack.config.js
@@ -13,27 +13,55 @@ module.exports = {
   //context: 执行上下文
 
   entry: {
-    main: './src/script/main.js',
-    main1: './src/script/main1.js'
+    main1: './src/script/main1.js',
+    main2: './src/script/main2.js',
+    main3: './src/script/main3.js'
   },
 
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'js/[name]-[chunkhash].js', //版本号，MD5值 ,js指定文件路径
-    publicPath: 'http://cdn.com/' //上线路径设置
+    //publicPath: 'http://cdn.com/' //上线路径设置
   },
 
   plugins: [
     new htmlWebpackPlugin({
-      filename: 'index.html',
+      filename: 'index1.html',
       template: 'index.html',
-      inject: false, //script标签放在哪里
-      title: 'webpack is headsome', //指定生成页面title
-      date: new Date(),
-      minify: {
-        removeComments: true,
-        collapseWhitespace: true
-      }
+      inject: 'body', //script标签放在哪里
+      title: 'index1', //指定生成页面title
+      // chunks: ['main1'] // 指定只引入哪个JS
+      // date: new Date(),
+      // minify: {
+      //   removeComments: true, //删除注释
+      //   collapseWhitespace: true //删除空格
+      // }
+    }),
+
+    new htmlWebpackPlugin({
+      filename: 'index2.html',
+      template: 'index.html',
+      inject: 'body', //script标签放在哪里
+      title: 'index2', //指定生成页面title
+      // chunks: ['main2']
+      // date: new Date(),
+      // minify: {
+      //   removeComments: true,
+      //   collapseWhitespace: true
+      // }
+    }),
+
+    new htmlWebpackPlugin({
+      filename: 'index3.html',
+      template: 'index.html',
+      inject: 'body', //script标签放在哪里
+      title: 'index3', //指定生成页面title
+      // chunks: ['main3']
+      // date: new Date(),
+      // minify: {
+      //   removeComments: true,
+      //   collapseWhitespace: true
+      // }
     })
   ]
 }
